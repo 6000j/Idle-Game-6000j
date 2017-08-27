4bcee53ec59176ae128d5f028704b39f23164d0a
6000j <31378570+6000j@users.noreply.github.com>
2017-08-27 18:45:21 +1000

    Added a basic mana system, no spells yet

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var gold = 0;
-var farms = 0;
 
+var maxMana = 1000;
+var mana = 1000;
+var manaRegen = 5;
+
+var farms = 0;
 var farmCostMulti = 1.15;
 var farmBaseProd = 1;
 var farmProdMulti = 1;
@@ -14,6 +18,16 @@ function goldClick(number){
 	document.getElementById("gold").innerHTML = gold;
 	};
 
+function manaGain(number) {
+	if(mana+manaRegen >= maxMana){
+		mana = maxMana;
+	}
+	else{
+		mana = mana + manaRegen;
+	}
+	document.getElementById("currentMana").innerHTML = mana;
+}	
+	
 function buyFarm(){
     var farmCost = Math.floor(10 * Math.pow(farmCostMulti,farms));     //works out the cost of this farm
     if(gold >= farmCost){                                   //checks that the player can afford the farm
@@ -39,11 +53,14 @@ function buyInn(){
 };
 
 window.setInterval(function tick(){
-	var farmProd = farms*farmBaseProd*farmProdMulti;
+	var farmProd = farms*farmBaseProd*farmProdMulti;			//building production and the like
 	document.getElementById('farmProd').innerHTML = farmProd;
 	goldClick(farmProd);
 	var innProd = inns*innBaseProd*innProdMulti;
 	document.getElementById('innProd').innerHTML = innProd;
 	goldClick(innProd);
+	
+	document.getElementById("maxMana").innerHTML = maxMana; //mana stuff
+	manaGain(manaRegen);
 
 }, 1000);
