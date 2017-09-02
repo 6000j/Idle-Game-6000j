cccbdcd2e84f08c655c0a3c15809de86d97ad85c
6000j <31378570+6000j@users.noreply.github.com>
2017-09-02 12:02:17 +1000

    ECS (not implemented, doesn't work)
    The basics of an ECS to make adding more content easier are in progress, the current one isn't linked to the html, as it isn't finished, but uploading to show progress.

---
+++ ECS.js
@@ -0,0 +1,53 @@
+Building.Entity = function Entity(){
+    this.id = Building.Entity.prototype._count;
+
+    // increment counter
+    Building.Entity.prototype._count++;
+
+    // The component data will live in this object
+    this.components = {};
+
+    return this;
+};
+// keep track of entities created
+Building.Entity.prototype._count = 0;
+
+Building.Entity.prototype.addComponent = function addComponent ( component ){
+    // Add component data to the entity
+    this.components[component.name] = component;
+    return this;
+};
+Building.Entity.prototype.removeComponent = function removeComponent ( componentName ){	
+    // Remove component data by removing the reference to it.
+    // Allows either a component function or a string of a component name to be
+    // passed in
+    var name = componentName; // assume a string was passed in
+
+    if(typeof componentName === 'function'){ 
+        // get the name from the prototype of the passed component function
+        name = componentName.prototype.name;
+    }
+
+    delete this.components[name];
+    return this;
+};
+
+Building.Entity.prototype.print = function print () {
+	// Function to print / log information about the entity
+    console.log(JSON.stringify(this, null, 4));
+    return this;
+};
+
+
+
+Building.Components.Production = function ComponentProduction ( value ){
+    value = value || 1;
+    this.value = value;
+
+    return this;
+};
+
+Building.Components.Production.prototype.name = 'Production:';
+
+var entity = new Building.Entity();
+entity.addComponent( new Building.Components.Production() );
